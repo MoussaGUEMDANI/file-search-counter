3be3d5cc1b6d9c8e49c60f54682d2f762c569a44
MoussaGUEMDANI <moussa.guemdani@outlook.com>
2026-09-18 14:28:04 +0200

    Add basic Tkinter window

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-def main():
-    print("File Search Counter")
+import tkinter as tk
 
+def main():
+    root = tk.Tk()
+
+    root.title("File Search Counter")
+    root.geometry("700x400")
+
+    title = tk.Label(
+        root,
+        text="File Search Counter", 
+        font=("Arial", 20, "bold"),
+    )
+
+    title.pack(pady=30)
+
+    root.mainloop()
+
+    
 if __name__ == "__main__":
     main()
